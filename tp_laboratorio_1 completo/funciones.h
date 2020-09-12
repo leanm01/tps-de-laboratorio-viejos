f0f85fe071dd9a00c21d25975c293a8d0a4660c5
leandro merino <60607751+leanm01@users.noreply.github.com>
2020-09-12 18:27:30 -0300

    Update funciones.h

--- tp_laboratorio_1 completo/funciones.h
+++ tp_laboratorio_1 completo/funciones.h
@@ -1,11 +1,51 @@
+
+/** \brief funcion que recibe los numeros que se ingresan
+ *
+ * \param primer operador recibe un dato float
+ * \param segund operador recibe un dato float
+ * \return devuelve los numeros ingresados con float
+ *
+ */
 float ingresarNumero(float dato);
-
+/** \brief funcion que suma los numeros que se ingresan
+ *
+ * \param primer operador recibe un dato float
+ * \param segundo operador recibe un dato float
+ * \return devuelve los numeros ingresados con float
+ *
+ */
 float sumaNumeros(float dato1 , float dato2);
-
+/** \brief funcion que resta los numeros que se ingresan
+ *
+ * \param primer operador recibe un dato float
+ * \param segundo operador recibe un dato float
+ * \return devuelve los numeros ingresados con float
+ *
+ */
 float restarNumeros(float dato1 , float dato2);
-
+/** \brief funcion que multiplica los numeros que se ingresan
+ *
+ * \param primer operador recibe un dato float
+ * \param segundo operador recibe un dato float
+ * \return devuelve los numeros ingresados con float
+ *
+ */
 float multiplicarNumeros( float dato1 , float dato2);
-
+/** \brief  funcion que divide los numeros que se ingresan
+ *
+ * \param primer operador recibe un dato float
+ * \param  segundo operador recibe un dato float
+ * \return devuelve los numeros ingresados con float
+ *
+ */
 float divisionNumeros( float operadorUno , float operadorDos);
+
+/** \brief funcion que factoriza los numeros que se ingresan
+ *
+ * \param primer operador recibe un dato float
+ * \param segundo operador recibe un dato float
+ * \return  devuelve los numeros ingresados con float
+ *
+ */
 
 float factorialPrimerOperador(float operadorUno);

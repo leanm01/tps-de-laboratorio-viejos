eb9048a3c45657a0130a86b342a2538966e94040
leandro merino <60607751+leanm01@users.noreply.github.com>
2021-05-10 18:00:22 -0300

    Update empleados.h

--- TPN2LABORATORIO/src/empleados.h
+++ TPN2LABORATORIO/src/empleados.h
@@ -19,21 +19,77 @@ typedef struct
 	int isEmpty;
 
 }eEmpleado;
+/** \brief estructura para el empleado
+ *
+ * \param
+ * \param
+ * \return 
+ *
+ */
 
 int initEmpleados(eEmpleado arrayEmpleado[], int tamArray);
-
+/** \brief funcion que inicializa empleados
+ *
+ * \param
+ * \param
+ * \return devuelve si se inicio o no un empleado
+ *
+ */
 int agregarEmpleados(eEmpleado arrayEmpleado[], int tamArray, int contador);
-
+/** \brief funcion que agrega o crea empleados
+ *
+ * \param
+ * \param
+ * \return devuelve al empleado que se agrego
+ *
+ */
 int mostrarEmpleados(eEmpleado arrayEmpleado[] ,int tamArray);
-
+/** \brief funcion que muestra empleados
+ *
+ * \param
+ * \param
+ * \return devuelve a los empleados para mostrarlos
+ *
+ */
 int mostrarEmpleado(eEmpleado arrayEmpleado[] ,int i);
-
+/** \brief funcion que muestra solo un empleado
+ *
+ * \param
+ * \param
+ * \return devuelve al empleado para mostrarlo
+ *
+ */
 int modificacionEmpleados(eEmpleado arrayEmpleado[], int tamArray);
-
+/** \brief funcion que modifica empleados
+ *
+ * \param
+ * \param
+ * \return devulve al empleado modificado
+ *
+ */
 int bajaEmpleado(eEmpleado arrayEmpleado[], int tamArray);
-
+/** \brief funcion que da de baja empleados
+ *
+ * \param
+ * \param
+ * \return devulve el espacio libre del empleado eliminado
+ *
+ */
 int ordenamientoEmpleados(eEmpleado arrayEmpleado[], int tamArray);
-
+/** \brief funcion que ordena empleados
+ *
+ * \param
+ * \param
+ * \return devuelve a los empleados ordenados
+ *
+ */
 void promedioTotal(eEmpleado arrayEmpleado[], int tamArray);
+/** \brief funcion que saca el promedio total
+ *
+ * \param
+ * \param
+ * \return devuelve el promedio total
+ *
+ */
 
 #endif /* EMPLEADOS_H_ */
